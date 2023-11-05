fd119a1ff5e40d8475e1442e260368fc732b67ba
Maksym Baldyniuk <frundzik1989@gmail.com>
2023-11-05 15:20:06 +0200

    task completed fixed

--- src/index.js
+++ src/index.js
@@ -1,8 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import { Provider } from 'react-redux';
-import { PersistGate } from 'redux-persist/integration/react';
-import { persistor, store } from 'redux/store';
+import { store } from 'redux/store';
 import { App } from 'components/App';
 import './index.css';
 
